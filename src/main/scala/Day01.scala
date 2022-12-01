b55328da49bff5cae377f764d4ab723049250d18
Katherine <katherine-fu@outlook.com>
2022-12-01 06:56:17 +0100

    cleanup

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -9,15 +9,17 @@ import scala.io.Source
 val input = Source.fromResource("Day01.txt").mkString
 
 def Day01Part1 =
-  val output = (for (s <- input.split("\n\n").toList) yield {
-      s.split("\n").map(_.toInt).toList}
+  val output = 
+    (for (s <- input.split("\n\n").toList) 
+    yield { s.split("\n").map(_.toInt).toList }
     ).filter(!_.isEmpty).map(_.sum).max
 
   println(s"Day 1 - part 1: $output")
 
 def Day01Part2 =
-  val output = (for (s <- input.split("\n\n").toList) yield {
-      s.split("\n").map(_.toInt).toList}
+  val output =
+    (for (s <- input.split("\n\n").toList) 
+    yield { s.split("\n").map(_.toInt).toList }
     ).filter(!_.isEmpty).map(_.sum).sorted.reverse.take(3).sum
 
   println(s"Day 1 - part 2: ${output}")
