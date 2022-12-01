fc593dbd6e9509d1810bb2b7b7c5c947730b7ffa
Katherine <katherine-fu@outlook.com>
2022-12-01 09:42:30 +0100

    prepare day2

---
+++ src/main/scala/Day02.scala
@@ -0,0 +1,13 @@
+package AOC2023
+
+import scala.io.Source
+
+@main def Day01 =
+  Day01Part1
+  Day01Part2
+
+val input = Source.fromResource("Example.txt").mkString
+
+def Day01Part1 = ???
+
+def Day01Part2 = ???
