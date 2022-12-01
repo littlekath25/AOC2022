d5271afe491d7f7a94d1868bd4b20440f777f9c3
Katherine <katherine-fu@outlook.com>
2022-12-01 06:40:15 +0100

    day

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -6,12 +6,18 @@ import scala.io.Source
   Day01Part1
   Day01Part2
 
-val input = Source.fromResource("Day01.txt").getLines.toList.map(_.toInt)
+val input = Source.fromResource("Day01.txt").mkString
 
 def Day01Part1 =
-  val answer = input.sliding(2, 1).count(l => l(0) < l(1))
-  println(s"Answer of day 01 - part 1: $answer")
+  val output = (for (s <- input.split("\n\n").toList) yield {
+      s.split("\n").map(_.toInt).toList}
+    ).filter(!_.isEmpty).map(_.sum).max
+
+  println(s"Day 1 - part 1: $output")
 
 def Day01Part2 =
-  val answer = input.sliding(3, 1).map(_.sum).sliding(2).count(l => l(0) < l(1))
-  println(s"Answer of day 01 - part 2: $answer")
+  val output = (for (s <- input.split("\n\n").toList) yield {
+      s.split("\n").map(_.toInt).toList}
+    ).filter(!_.isEmpty).map(_.sum).sorted.reverse.take(3).sum
+
+  println(s"Day 1 - part 2: ${output}")
