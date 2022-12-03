887abb0571bd3b0ebff4c86a0b913d93242e275e
Katherine <katherine-fu@outlook.com>
2022-12-03 08:08:25 +0100

    day03

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -2,19 +2,27 @@ package AOC2022
 import scala.io.Source
 
 object Day03 {
-  val input = Source.fromResource("Day03.txt")
+  val input = Source.fromResource("Day03.txt").getLines.toList
 
-  def Day03Part1 = 
-    val answer = ???
+  def toPoints(item: Char) : Int =
+    if (item.isLower)
+      item.toInt - 96
+    else
+      item.toInt - 38
+
+  def Day03Part1 =
+    val groupedByItem = input.map { items => val splitted = items.splitAt(items.length / 2); (splitted._1, splitted._2) }
+    val answer = groupedByItem.map(x => x._1.intersect(x._2).distinct).map(x => toPoints(x(0))).sum
 
     println(s"Day 3 - part 1: $answer")
 
-  def Day03Part2 = 
-    val answer = ???
+  def Day03Part2 =
+    val groupedByEleves = input.grouped(3)
+    val answer = groupedByEleves.flatMap(x => x(0).intersect(x(1).intersect(x(2))).distinct.map(toPoints)).sum
 
     println(s"Day 3 - part 2: $answer")
 
-  // def main(args: Array[String]): Unit =
-    // Day03Part1
-    // Day03Part2
+  def main(args: Array[String]): Unit =
+    Day03Part1
+    Day03Part2
 }
