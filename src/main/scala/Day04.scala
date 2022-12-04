c912e42c4a70313d9e4213a29d73f2d443fa7d7a
Katherine <katherine-fu@outlook.com>
2022-12-04 06:57:26 +0100

    da4

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -2,30 +2,25 @@ package AOC2022
 import scala.io.Source
 
 object Day04 {
-  val input = Source.fromResource("Day04.txt").getLines.map { x => 
-    val splitted = x.split(",")
+  val input = Source.fromResource("Day04.txt").getLines.map { pairs => 
+    val splitted = pairs.split(",")
     val one = splitted(0).split("-").map(_.toInt).toArray
     val two = splitted(1).split("-").map(_.toInt).toArray
     ((one(0) to one(1)), (two(0) to two(1)))
   }.toList
 
   def Day04Part1 =
-    val overlap = input.map { x => 
-      val hallo = x._1.intersect(x._2)
-      (hallo == x._1 || hallo == x._2)
-    }
-
-    val answer = overlap.filter(x => x == true).size
+    val answer = input.map { elves => 
+      val overlappingTasks = elves._1.intersect(elves._2)
+      (overlappingTasks == elves._1 || overlappingTasks == elves._2)
+    }.filter(_ == true).size
 
     println(s"Day 4 - part 1: $answer")
 
   def Day04Part2 =
-    val overlap = input.map {
-      x => 
-      x._1.intersect(x._2).nonEmpty
-    }
-
-    val answer = overlap.filter(x => x == true).size
+    val answer = input.map { elves =>
+      elves._1.intersect(elves._2).nonEmpty
+    }.filter(_ == true).size
 
     println(s"Day 4 - part 2: ${answer}")
 
