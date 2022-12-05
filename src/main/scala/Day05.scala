d2db5c8f4acfbcaa8b2857e4f284caa5c608b9a1
Katherine <katherine-fu@outlook.com>
2022-12-05 20:39:16 +0100

    mybox

--- src/main/scala/Day05.scala
+++ src/main/scala/Day05.scala
@@ -5,13 +5,13 @@ object Day05 {
   type MyBox = List[(BoxStack, Int)]
   case class BoxStack(boxes: List[Char])
 
-  val instructions = 
+  val instructions =
     Source.fromResource("Day05.txt")
     .getLines.map {
       case (s"move $box from $from to $to") => ((box.toInt, from.toInt, to.toInt))
     }.toList
 
-  val boxes = 
+  val boxes =
     Source.fromResource("Day05-Boxes.txt")
       .getLines
       .map(_.toList)
@@ -23,39 +23,35 @@ object Day05 {
         (BoxStack(row.filter(_ != ' ').slice(0, row.size - 1).reverse), row.last.asDigit)
       )
 
-  def moveBox(times: Int, from: Int, to: Int, boxes: MyBox) : MyBox =
+  def moveBox(times: Int, from: Int, to: Int, boxes: MyBox, reverse: Boolean) : MyBox =
     val copyOfOldBoxes = boxes(from - 1)._1.boxes
-    val boxesToMove = copyOfOldBoxes.slice(copyOfOldBoxes.size - times, copyOfOldBoxes.size)
+    var boxesToMove = copyOfOldBoxes.slice(copyOfOldBoxes.size - times, copyOfOldBoxes.size)
+
     boxes.map { currentBox =>
       if (currentBox._2 == from)
         val droppedBox = currentBox._1.boxes.slice(0, currentBox._1.boxes.size - times)
         (BoxStack(droppedBox), currentBox._2)
       else if (currentBox._2 == to)
+        if (reverse)
+          boxesToMove = boxesToMove.reverse
         val newStack = currentBox._1.boxes ::: boxesToMove
         (BoxStack(newStack), currentBox._2)
       else
         currentBox
     }
 
-  def repeatMoving(moves: Int)(from: Int, to: Int, input: MyBox) : MyBox =
-    if (moves > 0)
-      val newBoxes = moveBox(1, from, to, input)
-      repeatMoving(moves - 1)(from, to, newBoxes)
-    else
-      input
-
   def getAnswer(boxes: MyBox) : String =
     boxes.map((boxes, idx) => boxes.boxes.last).mkString
 
-  def Day05Part1 =
-    val newStackBoxes = instructions.foldLeft(boxes) { case ((mybox), (times, from, to)) => repeatMoving(times)(from, to, mybox) }
+  def solve(reverse: Boolean) : String =
+    val newStackBoxes = instructions.foldLeft(boxes) { case ((mybox), (times, from, to)) => moveBox(times, from, to, mybox, reverse) }
+    getAnswer(newStackBoxes)
 
-    println(s"Day 5 - part 1: ${getAnswer(newStackBoxes)}")
+  def Day05Part1 =
+    println(s"Day 5 - part 1: ${solve(true)}")
 
   def Day05Part2 =
-    val newStackBoxes = instructions.foldLeft(boxes) { case ((mybox), (times, from, to)) => moveBox(times, from, to, mybox) }
-
-    println(s"Day 5 - part 2: ${getAnswer(newStackBoxes)}")
+    println(s"Day 5 - part 2: ${solve(false)}")
 
   def main(args: Array[String]): Unit =
     Day05Part1
