201ed1b1ec94d3eb0843fd9a5024fdf48702062a
Katherine <katherine-fu@outlook.com>
2022-12-05 17:17:01 +0100

    refactor

--- src/main/scala/Day05.scala
+++ src/main/scala/Day05.scala
@@ -5,24 +5,26 @@ object Day05 {
   type MyBox = List[(BoxStack, Int)]
   case class BoxStack(boxes: List[Char])
 
-  val instructions = Source.fromResource("Day05.txt").getLines.map {
-    case (s"move ${box} from ${from} to ${to}") => ((box.toInt, from.toInt, to.toInt))
-  }.toList
+  val instructions = 
+    Source.fromResource("Day05.txt")
+    .getLines.map {
+      case (s"move $box from $from to $to") => ((box.toInt, from.toInt, to.toInt))
+    }.toList
 
-  val box1 = BoxStack(List('J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'))
-  val box2 = BoxStack(List('V', 'W', 'J'))
-  val box3 = BoxStack(List('G', 'V', 'L', 'J', 'B', 'T', 'H'))
-  val box4 = BoxStack(List('B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'))
-  val box5 = BoxStack(List('F', 'W', 'S', 'M', 'P', 'R', 'G'))
-  val box6 = BoxStack(List('G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'))
-  val box7 = BoxStack(List('D', 'H', 'G', 'M', 'R'))
-  val box8 = BoxStack(List('H', 'N', 'M', 'V', 'Z', 'D'))
-  val box9 = BoxStack(List('G', 'N', 'F', 'H'))
-
-  val boxes = List(box1, box2, box3, box4, box5, box6, box7, box8, box9).zipWithIndex
+  val boxes = 
+    Source.fromResource("Day05-Boxes.txt")
+      .getLines
+      .map(_.toList)
+      .toList
+      .transpose
+      .filter( row =>
+        row.exists(_.isLetterOrDigit)
+      ).map( row =>
+        (BoxStack(row.filter(_ != ' ').slice(0, row.size - 1).reverse), row.last.asDigit)
+      )
 
   def moveBox(times: Int, from: Int, to: Int, boxes: MyBox) : MyBox =
-    val copyOfOldBoxes = boxes.find(((_, idx) => idx == from)).get._1.boxes
+    val copyOfOldBoxes = boxes(from - 1)._1.boxes
     val boxesToMove = copyOfOldBoxes.slice(copyOfOldBoxes.size - times, copyOfOldBoxes.size)
     boxes.map { currentBox =>
       if (currentBox._2 == from)
@@ -46,12 +48,12 @@ object Day05 {
     boxes.map((boxes, idx) => boxes.boxes.last).mkString
 
   def Day05Part1 =
-    val newStackBoxes = instructions.foldLeft(boxes) { case ((mybox), (times, from, to)) => repeatMoving(times)(from - 1, to -1, mybox) }
+    val newStackBoxes = instructions.foldLeft(boxes) { case ((mybox), (times, from, to)) => repeatMoving(times)(from, to, mybox) }
 
     println(s"Day 5 - part 1: ${getAnswer(newStackBoxes)}")
 
   def Day05Part2 =
-    val newStackBoxes = instructions.foldLeft(boxes) { case ((mybox), (times, from, to)) => moveBox(times, from - 1, to -1, mybox) }
+    val newStackBoxes = instructions.foldLeft(boxes) { case ((mybox), (times, from, to)) => moveBox(times, from, to, mybox) }
 
     println(s"Day 5 - part 2: ${getAnswer(newStackBoxes)}")
 
