e3e0f8c18ebffdd7b42dbbb92c35c3dd0317a319
Katherine <katherine-fu@outlook.com>
2022-12-09 14:12:43 +0100

    damn

--- src/main/scala/Day09.scala
+++ src/main/scala/Day09.scala
@@ -4,56 +4,61 @@ import scala.collection.mutable
 
 object Day09 {
 
-  // def Day09Part1 =
-  //   val input = Source.fromResource("Example.txt").getLines.map{ instr => val splitted = instr.split(" "); (splitted(0), splitted(1).toInt)}.toList
-  //   val max = 1000
+  def Day09Part1 =
+    def checkIfTouching(tail: (Int, Int), head: (Int, Int)): Boolean =
+      val difference = ((tail._1 - head._1).abs, (tail._2 - head._2).abs)
+      val touchingOrNot = !(difference._1 > 1 || difference._2 > 1)
+      touchingOrNot
 
-  //   def move(from: (Int, Int), to: (Int, Int)): (Int, Int) =
-  //     if (from._1 < to._1)
-  //       (from._1 + 1, from._2)
-  //     else if (from._1 > to._1)
-  //       (from._1 - 1, from._2)
-  //     else if (from._2 < to._2)
-  //       (from._1, from._2 + 1)
-  //     else
-  //       (from._1, from._2 - 1)
+    val input = Source.fromResource("Example.txt").getLines.map{ instr => val splitted = instr.split(" "); (splitted(0), splitted(1).toInt)}.toList
+    val max = 1000
 
-  //   var bridgeMap = Array.tabulate(max, max)((x, y) => ("."))
-  //   val start = (max - 1, 0)
+    def move(from: (Int, Int), to: (Int, Int)): (Int, Int) =
+      if (from._1 < to._1)
+        (from._1 + 1, from._2)
+      else if (from._1 > to._1)
+        (from._1 - 1, from._2)
+      else if (from._2 < to._2)
+        (from._1, from._2 + 1)
+      else
+        (from._1, from._2 - 1)
 
-  //   val head = (500, 500)
-  //   val tail = head
+    var bridgeMap = Array.tabulate(max, max)((x, y) => ("."))
+    val start = (max - 1, 0)
 
-  //   def getNewCoords(direction: Char, moves: Int, current: (Int, Int)): (Int, Int) =
-  //     if (direction == 'U')
-  //       (current._1 - moves, current._2)
-  //     else if (direction == 'D')
-  //       (current._1 + moves, current._2)
-  //     else if (direction == 'L')
-  //       (current._1, current._2 - moves)
-  //     else
-  //       (current._1, current._2 + moves)
+    val head = (500, 500)
+    val tail = head
+
+    def getNewCoords(direction: Char, moves: Int, current: (Int, Int)): (Int, Int) =
+      if (direction == 'U')
+        (current._1 - moves, current._2)
+      else if (direction == 'D')
+        (current._1 + moves, current._2)
+      else if (direction == 'L')
+        (current._1, current._2 - moves)
+      else
+        (current._1, current._2 + moves)
     
-  //   def moveUntil(bridgeMap: Array[Array[String]], head: (Int, Int), tail: (Int, Int), to: (Int, Int)): (Array[Array[String]], (Int, Int)) =
-  //     bridgeMap(tail._1)(tail._2) = "#"
-  //     if (head != to)
-  //       val newHead = move(head, to)
-  //       if (!checkIfTouching(tail, newHead))
-  //         val newTail = head
-  //         moveUntil(bridgeMap, newHead, newTail, to)
-  //       else
-  //         moveUntil(bridgeMap, newHead, tail, to)
-  //     else
-  //       (bridgeMap, tail)
+    def moveUntil(bridgeMap: Array[Array[String]], head: (Int, Int), tail: (Int, Int), to: (Int, Int)): (Array[Array[String]], (Int, Int)) =
+      bridgeMap(tail._1)(tail._2) = "#"
+      if (head != to)
+        val newHead = move(head, to)
+        if (!checkIfTouching(tail, newHead))
+          val newTail = head
+          moveUntil(bridgeMap, newHead, newTail, to)
+        else
+          moveUntil(bridgeMap, newHead, tail, to)
+      else
+        (bridgeMap, tail)
 
-  //   val answer = input.foldLeft(bridgeMap, head, tail) {
-  //     case ((bridgemap, head, tail), (direction, moves)) =>
-  //       val newHeadCoords = getNewCoords(direction(0), moves, head)
-  //       val newBridgeMapAndCoords = moveUntil(bridgeMap, head, tail, newHeadCoords)
-  //       ((newBridgeMapAndCoords._1, newHeadCoords, newBridgeMapAndCoords._2))
-  //   }
+    val answer = input.foldLeft(bridgeMap, head, tail) {
+      case ((bridgemap, head, tail), (direction, moves)) =>
+        val newHeadCoords = getNewCoords(direction(0), moves, head)
+        val newBridgeMapAndCoords = moveUntil(bridgeMap, head, tail, newHeadCoords)
+        ((newBridgeMapAndCoords._1, newHeadCoords, newBridgeMapAndCoords._2))
+    }
 
-    // println(s"Day 08 - part 1: ${answer._1.flatten.toList.count(_ == "#")}")
+    println(s"Day 08 - part 1: ${answer._1.flatten.toList.count(_ == "#")}")
 
   def Day09Part2 =
     case class Move(direction: String, moves: Int)
