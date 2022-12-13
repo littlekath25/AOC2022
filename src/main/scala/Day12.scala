a078ab4f8063e2e2e82d32eadd643139dd0bd9fe
Katherine <katherine-fu@outlook.com>
2022-12-13 09:56:41 +0100

    ok

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -3,10 +3,9 @@ import scala.io.Source
 import scala.collection.mutable._
 
 object Day12 {
-
   case class Pos(y: Int, x: Int)
-
   var input = Source.fromResource("Day12.txt").getLines.map(_.toArray).toArray
+  val coords = (0 to 40).flatMap ( y => (0 to 112).map (x => (y, x))).toArray
 
   val start = input.map { row =>
     if (row.contains(83))
@@ -22,28 +21,14 @@ object Day12 {
       Pos(0, 0)
   }.filter(pos => !(pos.x == 0 && pos.y == 0)).head
 
-  println(s"$start - $dest")
-
-  var grid = Source.fromResource("Day12.txt").getLines.map{
-    _.toArray.map{ x => 
-      if (x == 'E')
-        122
-      else if (x == 'S')
-        97
-      else x.toInt}
-  }.toArray
-
-  val max = (grid.size, grid.head.size)
-
-  val visited = grid.map(line => line.map(_ => false))
-  var shortestPath = grid.map(line => line.map(x => 1000))
-
+  var gridWithHeight = input.map{_.map {x => if (x == 'E') 122 else if (x == 'S') 97 else x.toInt}}.toArray
+  val visited = gridWithHeight.map(line => line.map(_ => false))
+  val shortestPath = gridWithHeight.map(line => line.map(x => 1000))
   shortestPath(dest.y)(dest.x) = 0
-
   var queue = scala.collection.mutable.Queue(dest)
 
   def checkIfOutOfBounds(pos: Pos): Boolean =
-    (pos.y >= 0 && pos.y < max._1) && (pos.x >= 0 && pos.x < max._2)
+    (pos.y >= 0 && pos.y < gridWithHeight.size) && (pos.x >= 0 && pos.x < gridWithHeight.head.size)
 
   def getShortestPath(queue: scala.collection.mutable.Queue[Pos], shortestPath: Array[Array[Int]], visited: Array[Array[Boolean]], size: Int) =
     while (queue.size > 0) {
@@ -62,8 +47,8 @@ object Day12 {
       neighbours = neighbours.filter(neighbour => checkIfOutOfBounds(neighbour))
 
       neighbours.foreach { neighbour =>
-        val currentHeight = grid(current.y)(current.x)
-        val nextHeight = grid(neighbour.y)(neighbour.x)
+        val currentHeight = gridWithHeight(current.y)(current.x)
+        val nextHeight = gridWithHeight(neighbour.y)(neighbour.x)
 
         if (currentHeight >= nextHeight - 1)
           val shortestDist = (shortestPath(neighbour.y)(neighbour.x)) + 1
@@ -82,13 +67,11 @@ object Day12 {
     println(s"Day 12 - part 1: ${shortestPath(start.y)(start.x)}")
 
   def Day12Part2 =
-    val minimum = Int.MaxValue
-    val coords = (0 to 40).flatMap ( y => (0 to 112).map (x => (y, x))).toArray
     val zipped = input.flatten.zip(coords).zip(shortestPath.flatten).filter(value => value._1._1 == 'a' ||  value._1._1 == 'S').map(value => value._2).min
 
     println(s"Day 12 - part 2: ${zipped}")
 
-  def main(args: Array[String]): Unit =
-    Day12Part1
-    Day12Part2
+  // def main(args: Array[String]): Unit =
+  //   Day12Part1
+  //   Day12Part2
 }
--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -1,20 +1,90 @@
 package AOC2022
 import scala.io.Source
+import scala.collection.mutable.Stack
 
 object Day13 {
-  val input = Source.fromResource("Example.txt").getLines
+  val file = Source.fromResource("Day13.txt").getLines.toList
+  var firstQueue = scala.collection.mutable.Queue[Char]()
+  var secondQueue = scala.collection.mutable.Queue[Char]()
+
+  def clearQueue =
+    while (firstQueue.nonEmpty)
+      firstQueue.dequeue
+    while (secondQueue.nonEmpty)
+      secondQueue.dequeue
+
+  def queueItems(first: String, second: String) =
+    clearQueue
+
+    first.foreach { value =>
+        if (value == '[' || value == ']' || value.isDigit || value == ':')
+          firstQueue.enqueue(value)
+      }
+
+    second.foreach { value =>
+      if (value == '[' || value == ']' || value.isDigit || value == ':')
+        secondQueue.enqueue(value)
+    }
+
+  def dequeueBoth =
+    firstQueue.dequeue
+    secondQueue.dequeue
+
+  def addListItemAfter(queue: scala.collection.mutable.Queue[Char]): scala.collection.mutable.Queue[Char] =
+    val value = queue.front
+    scala.collection.mutable.Queue[Char](queue.front, ']').enqueueAll(queue.tail)
+
+  def solve(left: String, right: String): Boolean =
+    var inOrder = false
+    val first = left.replace("10", ":")
+    val second = right.replace("10", ":")
+
+    queueItems(first, second)
+
+    while (firstQueue.nonEmpty && secondQueue.nonEmpty)
+
+      if (firstQueue.front == secondQueue.front)
+        dequeueBoth
+      else if (firstQueue.front == ']' && secondQueue.front != ']')
+        inOrder = true
+        clearQueue
+      else if (firstQueue.front != ']' && secondQueue.front == ']')
+        clearQueue
+      else if (firstQueue.front == '[' && secondQueue.front != '[')
+        firstQueue.dequeue
+        secondQueue = addListItemAfter(secondQueue)
+      else if (firstQueue.front != '[' && secondQueue.front == '[')
+        secondQueue.dequeue
+        firstQueue = addListItemAfter(firstQueue)
+      else if ((firstQueue.front.isDigit || firstQueue.front == ':') && (secondQueue.front.isDigit || secondQueue.front == ':'))
+        if (firstQueue.front.toInt < secondQueue.front.toInt)
+          inOrder = true
+          clearQueue
+        else if (firstQueue.front.toInt > secondQueue.front.toInt)
+          clearQueue
+
+    if (firstQueue.isEmpty && secondQueue.nonEmpty)
+      inOrder = true
+    inOrder
 
   def Day13Part1 =
-    val answer = ???
+    val input = file.filter(_.nonEmpty).grouped(2).toList.map(pair => (pair.head, pair.last))
+    val calculatedList = input.map((first, second) => solve(first, second)).zipWithIndex
+    val onlyTrue = calculatedList.filter(x => x._1 == true).map(_._2 + 1)
 
-    println(s"Day 13 - part 1: ${input}")
+    println(s"Day 13 - part 1: ${onlyTrue.sum}")
+    clearQueue
 
   def Day13Part2 =
-    val answer = ???
+    val newFile = file.filter(_.nonEmpty) ::: List("[[2]]", "[[6]]")
+    val calculatedList = newFile.sortWith((first, second) => solve(first, second)).zipWithIndex
 
-    println(s"Day 13 - part 2: ${answer}")
+    val indexOf2 = calculatedList.find((str, idx) => str == "[[2]]").get._2 + 1
+    val indexOf6 = calculatedList.find((str, idx) => str == "[[6]]").get._2 + 1
 
-  // def main(args: Array[String]): Unit =
-  //   Day13Part1
-  //   Day13Part2
+    println(s"Day 13 - part 2: ${indexOf2 * indexOf6}")
+
+  def main(args: Array[String]): Unit =
+    Day13Part1
+    Day13Part2
 }
