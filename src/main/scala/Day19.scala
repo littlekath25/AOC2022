d5eebc2cbb20c22fa7a8df5bbd94b9868d700232
Katherine <katherine-fu@outlook.com>
2022-12-19 23:15:59 +0100

    hmmmm

--- src/main/scala/Day19.scala
+++ src/main/scala/Day19.scala
@@ -8,39 +8,47 @@ object Day19 {
     case ClayRobot
     case ObsidianRobot
     case GeodeRobot
+    case Nothing
 
-  final case class Blueprint(minerals: Minerals, robots: List[Robot], shop: Map[RobotType, Minerals]) {
+  import RobotType._
+
+  final case class Robot(version: RobotType)
+  final case class Blueprint(minerals: Minerals, robots: List[RobotType], shop: Map[RobotType, Minerals]) {
+    val highestOreCost = shop.map{item => item._2.ore}.max
+
     def harvest: Minerals =
-      val collected = robots.map { robot =>
-        robot.version match {
-        case (RobotType.OreRobot) => Minerals(1, 0, 0, 0)
-        case (RobotType.ClayRobot) => Minerals(0, 1, 0, 0)
-        case (RobotType.ObsidianRobot) => Minerals(0, 0, 1, 0)
-        case (RobotType.GeodeRobot) => Minerals(0, 0, 0, 1)
+      robots.map { robot =>
+        robot match {
+        case OreRobot       => Minerals(1, 0, 0, 0)
+        case ClayRobot      => Minerals(0, 1, 0, 0)
+        case ObsidianRobot  => Minerals(0, 0, 1, 0)
+        case GeodeRobot     => Minerals(0, 0, 0, 1)
+        case Nothing        => minerals
       }}.foldLeft(Minerals(0, 0, 0, 0))(_ plus _)
-      collected
-
-    def getProduction: (Int, Int, Int) =
-      (robots.count(_.version == RobotType.OreRobot),
-       robots.count(_.version == RobotType.ClayRobot),
-       robots.count(_.version == RobotType.ObsidianRobot)
-      )
-
-    def isEnoughToBuild(robot: RobotType): Boolean =
-      val enough = (shop(robot).ore <= minerals.ore && shop(robot).clay <= minerals.clay && shop(robot).obsidian <= minerals.obsidian)
-      enough
-
-    def producibleRobots: List[RobotType] =
-      var listOfAvailableBuilds = List[RobotType]()
-
-      shop.map { robot =>
-        if (isEnoughToBuild(robot._1))
-          listOfAvailableBuilds = listOfAvailableBuilds.appended(robot._1)
+ 
+    def buildRobot(action: RobotType): List[RobotType] =
+      action match {
+        case OreRobot      => robots.concat(List(OreRobot))
+        case ClayRobot     => robots.concat(List(ClayRobot))
+        case ObsidianRobot => robots.concat(List(ObsidianRobot))
+        case GeodeRobot    => robots.concat(List(GeodeRobot))
+        case Nothing       => robots
       }
-      listOfAvailableBuilds
-      
-    def buildRobot(robot: RobotType): List[Robot] =
-      this.robots.concat(List(Robot(robot)))
+
+    def generateActions: List[RobotType] =
+      val oreRobot: Option[RobotType] = if (minerals.ore >= shop(OreRobot).ore && robots.count(_ == OreRobot) <= (highestOreCost)) Some(OreRobot) else None
+      val clayRobot: Option[RobotType] = if (minerals.ore >= shop(ClayRobot).ore && robots.count(_ == ClayRobot) <= shop(ObsidianRobot).clay) Some(ClayRobot) else None
+      val obsidianRobot: Option[RobotType] = if (minerals.ore >= shop(ObsidianRobot).ore && minerals.clay >= shop(ObsidianRobot).clay) Some(ObsidianRobot) else None
+      val geodeRobot: Option[RobotType] = if (minerals.ore >= shop(GeodeRobot).ore && minerals.obsidian >= shop(GeodeRobot).obsidian) Some(GeodeRobot) else None
+
+      if (minerals.obsidian >= shop(GeodeRobot).obsidian)
+        List(geodeRobot, Some(Nothing)).flatten
+      else if (minerals.clay >= shop(ObsidianRobot).clay)
+        List(geodeRobot, obsidianRobot, Some(Nothing)).flatten
+      else if (minerals.ore > highestOreCost)
+        List(oreRobot, clayRobot, obsidianRobot, geodeRobot).flatten
+      else
+        List(oreRobot, clayRobot, obsidianRobot, geodeRobot, Some(Nothing)).flatten
     }
 
   final case class Minerals(ore: Int, clay: Int, obsidian: Int, geode: Int) {
@@ -51,62 +59,56 @@ object Day19 {
       Minerals(this.ore - other.ore, this.clay - other.clay, this.obsidian - other.obsidian, this.geode)
   }
 
-  final case class Robot(version: RobotType)
-  final case class Item(item: RobotType, cost: Minerals)
-
-  val blueprints = Source.fromResource("Example.txt").getLines.map { line => line match
+  val blueprints = Source.fromResource("Day19.txt").getLines.map { line => line match
     case (s"Blueprint $name: Each ore robot costs $ore ore. Each clay robot costs $clay ore. Each obsidian robot costs $obsidianOre ore and $obsidianClay clay. Each geode robot costs $geodeOre ore and $geodeObsidian obsidian.") =>
       (name.toInt -> Blueprint(
         Minerals(0, 0, 0, 0),
-        List(Robot(RobotType.OreRobot)),
+        List(OreRobot),
         Map(
-          (RobotType.OreRobot -> Minerals(ore.toInt, 0, 0, 0)),
-          (RobotType.ClayRobot -> Minerals(clay.toInt, 0, 0, 0)),
-          (RobotType.ObsidianRobot -> Minerals(obsidianOre.toInt, obsidianClay.toInt, 0, 0)),
-          (RobotType.GeodeRobot -> Minerals(geodeOre.toInt, 0, geodeObsidian.toInt, 0))
+          (OreRobot -> Minerals(ore.toInt, 0, 0, 0)),
+          (ClayRobot -> Minerals(clay.toInt, 0, 0, 0)),
+          (ObsidianRobot -> Minerals(obsidianOre.toInt, obsidianClay.toInt, 0, 0)),
+          (GeodeRobot -> Minerals(geodeOre.toInt, 0, geodeObsidian.toInt, 0))
         )
       ))
   }to(scala.collection.mutable.Map)
 
-  def oneRound(blueprint: Blueprint, limit: Int): Blueprint = 
-    var collectedMinerals = blueprint.minerals
-    var newBlueprint = blueprint
-    if (limit > 0)
+  def getMaxGeodes(blueprint: Blueprint, currentMax: Int, limit: Int): Int = 
+    val newMax =
+      if (limit == 0 && (blueprint.minerals.geode + blueprint.robots.count(_ == GeodeRobot)) > currentMax) 
+        (blueprint.minerals.geode + blueprint.robots.count(_ == GeodeRobot))
+      else
+        currentMax
 
-      val production = newBlueprint.getProduction
-      val producible = newBlueprint.producibleRobots
-
-      if (producible.contains(RobotType.GeodeRobot))
-        newBlueprint = blueprint.copy(minerals = newBlueprint.minerals.minus(newBlueprint.shop(RobotType.GeodeRobot)), robots = newBlueprint.buildRobot(RobotType.GeodeRobot))
-      else if (producible.contains(RobotType.ObsidianRobot) && newBlueprint.shop(RobotType.GeodeRobot).obsidian - 1 > production._3)
-        newBlueprint = blueprint.copy(minerals = newBlueprint.minerals.minus(newBlueprint.shop(RobotType.ObsidianRobot)), robots = newBlueprint.buildRobot(RobotType.ObsidianRobot))
-      else if (producible.contains(RobotType.OreRobot) && newBlueprint.shop(RobotType.ObsidianRobot).ore -1 > production._1)
-        newBlueprint = blueprint.copy(minerals = newBlueprint.minerals.minus(newBlueprint.shop(RobotType.OreRobot)), robots = newBlueprint.buildRobot(RobotType.OreRobot))
-      else if (producible.contains(RobotType.ClayRobot) && newBlueprint.shop(RobotType.ObsidianRobot).clay / 3 - 1 > production._2)
-        newBlueprint = blueprint.copy(minerals = newBlueprint.minerals.minus(newBlueprint.shop(RobotType.ClayRobot)), robots = newBlueprint.buildRobot(RobotType.ClayRobot))
-
-      // println(s"AFTER BUILDING ROBOT: ${newBlueprint.minerals}")
-      collectedMinerals = blueprint.harvest.plus(newBlueprint.minerals)
-      // println(s"ROBOTS: ${blueprint.robots}")
-      // println(s"NEW AFTER HARVEST: ${collectedMinerals}")
-
-      newBlueprint = newBlueprint.copy(minerals = collectedMinerals)
-      oneRound(newBlueprint, limit - 1)
-    
-    else
-      newBlueprint
+    if (limit == 0)
+      blueprint.minerals.geode + blueprint.robots.count(_ == GeodeRobot)
+    else 
+      val newActions = blueprint.generateActions
+      newActions.foldLeft(newMax)((max, action) =>
+        if (action == Nothing)
+          val harvestedMinerals = blueprint.harvest.plus(blueprint.minerals)
+          val possibleScore = getMaxGeodes(blueprint.copy(minerals = harvestedMinerals), newMax, limit - 1)
+          if (possibleScore > max) 
+            possibleScore
+          else
+            max
+        else
+          val newRobots = blueprint.buildRobot(action)
+          val newMinerals = blueprint.minerals.minus(blueprint.shop(action))
+          val harvestedMinerals = blueprint.harvest.plus(newMinerals)
+          val possibleScore = getMaxGeodes(blueprint.copy(minerals = harvestedMinerals, robots = newRobots), newMax, limit - 1)
+          if (possibleScore > max) possibleScore else max
+      )
 
   def Day19Part1 =
-    val answer = oneRound(blueprints(1), 24)
+    val answer = blueprints.map(print => getMaxGeodes(print._2, 0, 23) * print._1).sum
+    println(s"Day 19 - part 1: ${answer}")
 
-    println(s"Day 19 - part 1: ${answer.minerals}")
+  def Day19Part2 =
+    val answer = blueprints.take(3).map(print => getMaxGeodes(print._2, 0, 31)).product
+    println(s"Day 19 - part 2: ${answer}")
 
-  // def Day19Part2 =
-  //   val answer = ???
-
-  //   println(s"Day 19 - part 2: ${answer}")
-
-  def main(args: Array[String]): Unit =
-    Day19Part1
+  // def main(args: Array[String]): Unit =
+  //   Day19Part1
   //   Day19Part2
 }
--- src/main/scala/Template.scala
+++ src/main/scala/Template.scala
@@ -1,102 +1,20 @@
-package aoc19
-
+package AOC2022
 import scala.io.Source
 
-case class Blueprint(index: Int, oreRobotOre: Int, clayRobotOre: Int, obsidianRobotOre: Int, obsidianRobotClay: Int, geodeRobotOre: Int, geodeRobotObsidian: Int):
-  val highestOreCost = List(oreRobotOre, clayRobotOre, obsidianRobotOre, geodeRobotOre).max
+object Day00 {
+  val input = Source.fromResource("Example.txt").getLines
 
-case class MaterialStash(ore: Int = 0, clay: Int = 0, obsidian: Int = 0, geode: Int = 0)
+  def Day00Part1 =
+    val answer = ???
 
-sealed trait Robot
-case object OreRobot extends Robot
-case object ClayRobot extends Robot
-case object ObsidianRobot extends Robot
-case object GeodeRobot extends Robot
+    println(s"Day 00 - part 1: ${input}")
 
-sealed trait Action
-case object DoNothing extends Action
-case object BuildOreRobot extends Action
-case object BuildClayRobot extends Action
-case object BuildObsidianRobot extends Action
-case object BuildGeodeRobot extends Action
+  def Day00Part2 =
+    val answer = ???
 
-def parseBlueprint(s: String): Blueprint = {
-  s match {
-    case s"Blueprint ${index}: Each ore robot costs ${oro} ore. Each clay robot costs ${cro} ore. Each obsidian robot costs ${obro} ore and ${obrc} clay. Each geode robot costs ${gror} ore and ${grob} obsidian." => Blueprint(index.toInt, oro.toInt, cro.toInt, obro.toInt, obrc.toInt, gror.toInt, grob.toInt)
-    case _ => sys.error("booboo")
-  }
+    println(s"Day 00 - part 2: ${answer}")
+
+  // def main(args: Array[String]): Unit =
+  //   Day00Part1
+  //   Day00Part2
 }
-
-def checkBuildRobot(blueprint: Blueprint, material: MaterialStash, robots: List[Robot]): List[Action] = {
-  val oreRobot: Option[Action] = if (material.ore >= blueprint.oreRobotOre && robots.count(_ == OreRobot) <= (blueprint.highestOreCost)) Some(BuildOreRobot) else None
-  val clayRobot: Option[Action] = if (material.ore >= blueprint.clayRobotOre && robots.count(_ == ClayRobot) <= (blueprint.obsidianRobotClay)) Some(BuildClayRobot) else None
-  val obsidianRobot: Option[Action] = if (material.ore >= blueprint.obsidianRobotOre && material.clay >= blueprint.obsidianRobotClay) Some(BuildObsidianRobot) else None
-  val geodeRobot: Option[Action] = if (material.ore >= blueprint.geodeRobotOre && material.obsidian >= blueprint.geodeRobotObsidian) Some(BuildGeodeRobot) else None
-
-  if (material.obsidian >= blueprint.geodeRobotObsidian) {
-    List(geodeRobot, Some(DoNothing)).flatten
-  } else if (material.clay >= blueprint.obsidianRobotClay) {
-    List(geodeRobot, obsidianRobot, Some(DoNothing)).flatten
-  } else if (material.ore > blueprint.highestOreCost) {
-    List(oreRobot, clayRobot, obsidianRobot, geodeRobot).flatten
-  } else {
-    List(oreRobot, clayRobot, obsidianRobot, geodeRobot, Some(DoNothing)).flatten
-  }
-}
-
-def buildRobot(action: Action): Robot =
-  action match {
-    case BuildOreRobot      => OreRobot
-    case BuildClayRobot     => ClayRobot
-    case BuildObsidianRobot => ObsidianRobot
-    case BuildGeodeRobot    => GeodeRobot
-  }
-
-def produceMaterial(robots: List[Robot], stash: MaterialStash): MaterialStash = {
-  robots match {
-    case h :: t => h match {
-      case OreRobot       => produceMaterial(t, stash.copy(ore = stash.ore + 1))
-      case ClayRobot      => produceMaterial(t, stash.copy(clay = stash.clay + 1))
-      case ObsidianRobot  => produceMaterial(t, stash.copy(obsidian = stash.obsidian + 1))
-      case GeodeRobot     => produceMaterial(t, stash.copy(geode = stash.geode + 1))
-    }
-    case Nil => stash
-  }
-}
-
-def consumeMaterial(action: Action, blueprint: Blueprint, stash: MaterialStash): MaterialStash = {
-  action match {
-    case BuildOreRobot      => stash.copy(ore = stash.ore - blueprint.oreRobotOre)
-    case BuildClayRobot     => stash.copy(ore = stash.ore - blueprint.clayRobotOre)
-    case BuildObsidianRobot => stash.copy(ore = stash.ore - blueprint.obsidianRobotOre, clay = stash.clay - blueprint.obsidianRobotClay)
-    case BuildGeodeRobot    => stash.copy(ore = stash.ore - blueprint.geodeRobotOre, obsidian = stash.obsidian - blueprint.geodeRobotObsidian)
-  }
-}
-
-def maxGeodeCollection(blueprint: Blueprint, robots: List[Robot], timeLimit: Int, material: MaterialStash = MaterialStash(), curMax: Int = 0, currentTime: Int = 1): Int =
-  val newMax = if (currentTime == timeLimit && (material.geode + robots.count(_ == GeodeRobot)) > curMax) {material.geode + robots.count(_ == GeodeRobot)} else curMax
-  if (currentTime == timeLimit) material.geode + robots.count(_ == GeodeRobot) else {
-    val newActions = checkBuildRobot(blueprint, material, robots)
-    newActions.foldLeft(newMax)((max, action) =>
-      if (action == DoNothing) {
-        val newMaterial = produceMaterial(robots, material)
-        val possibleScore = maxGeodeCollection(blueprint, robots, timeLimit, newMaterial, newMax, currentTime + 1)
-        if (possibleScore > max) {possibleScore} else {max}
-      } else {
-        val newRobot = buildRobot(action)
-        val newMaterial = consumeMaterial(action, blueprint, produceMaterial(robots, material))
-        val possibleScore = maxGeodeCollection(blueprint, newRobot :: robots, timeLimit, newMaterial, newMax, currentTime + 1)
-        if (possibleScore > max) {possibleScore} else {max}}
-    )
-  }
-
-object AOC19 extends App:
-  val input = Source.fromFile("src/main/resources/input_aoc19.txt").getLines.map(parseBlueprint).toList
-
-  val answerP1 = input.map(b => maxGeodeCollection(b, List(OreRobot), timeLimit = 24) * b.index).sum
-
-  val answerP2 = input.take(3).map(b => maxGeodeCollection(b, List(OreRobot), timeLimit = 32)).product
-
-  println("Answer to part 1: " + answerP1)
-  println("Answer to part 2: " + answerP2)
-
