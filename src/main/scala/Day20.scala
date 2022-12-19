cc14fda1d738bd3d905e6d9ef64fe019deda9914
Katherine <katherine-fu@outlook.com>
2022-12-19 23:16:46 +0100

    prepare

---
+++ src/main/scala/Day20.scala
@@ -0,0 +1,20 @@
+package AOC2022
+import scala.io.Source
+
+object Day20 {
+  val input = Source.fromResource("Example.txt").getLines
+
+  def Day20Part1 =
+    val answer = ???
+
+    println(s"Day 20 - part 1: ${input}")
+
+  def Day20Part2 =
+    val answer = ???
+
+    println(s"Day 20 - part 2: ${answer}")
+
+  // def main(args: Array[String]): Unit =
+  //   Day20Part1
+  //   Day20Part2
+}
