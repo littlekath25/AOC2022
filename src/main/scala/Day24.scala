9d18701c6163daeb9d2e8ade2a17362d3ba7c918
Katherine <katherine-fu@outlook.com>
2022-12-23 15:02:49 +0100

    prepare yourself

---
+++ src/main/scala/Day24.scala
@@ -0,0 +1,18 @@
+package AOC2022
+import scala.io.Source
+object Day00 {
+  val input = Source.fromResource("Example.txt").getLines
+
+  def Day00Part1 =
+
+    println(s"Day 00 - part 1: ${input}")
+
+  def Day00Part2 =
+    val answer = ???
+
+    println(s"Day 00 - part 2: ${answer}")
+
+  // def main(args: Array[String]): Unit =
+  //   Day00Part1
+  //   Day00Part2
+}
